24bd78383df14b13b9537b40e8d978ebdbf01d69
Fırat Atalay <firatatalay34@hotmail.com>
2020-08-12 11:38:04 +0300

    New Project was created
    The project was cleaned for new project.
    routeManager was created for more dynamic.

--- app.js
+++ app.js
@@ -1,7 +1,4 @@
-/* var http = require('http'); */
 var fs = require('fs');
-
-
 
 var  express = require('express');
 var app = express();
@@ -11,36 +8,9 @@ var ejsLayouts = require('express-ejs-layouts');
 app.set('view engine', 'ejs');
 app.set('views',path.join(__dirname, '/app_server/views'));
 
-var routeElektronik = require('./app_server/routes/elektronikRoute');
 app.use(ejsLayouts);
 app.use('/public',  express.static(path.join(__dirname, 'public')))
 
-app.use(function(req,res,next){
-    console.log("url...:" + req.url)
-    console.log("time..:" + Date.now());
-    next();
-});
+require('./app_server/routes/routeManager')(app)
 
-app.use('/elektronik',routeElektronik)
-
-/*app.get('/elektronik', ctrlElektronik.index);
-app.get('/elektronik/bilgisayar', ctrlElektronik.bilgisayar); */
-
-/*
-var yonlendirici = new Object();
-
-var homeController = function(req, res){
-}
-var  loginController = function(req, res){ 
-}
-
-yonlendirici['/'] = homeController;
-yonlendirici['/login'] = loginController; 
-
-var server = http.createServer(function(req,res){
-
-   if(req.url in yonlendirici)
-        yonlendirici[req.url](req, res);
-});
-*/
 app.listen(8000);
