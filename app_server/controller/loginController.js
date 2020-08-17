de0e0b9485fc151ff9cb02e179157df786ad8966
Fırat Atalay <firatatalay34@hotmail.com>
2020-08-17 15:27:53 +0300

    User password has  been hashed.

--- app_server/controller/loginController.js
+++ app_server/controller/loginController.js
@@ -1,5 +1,7 @@
 var Kullanici = require('../models/kullanici')
-
+var bcrypt = require('bcrypt');
+const { db } = require('../models/kullanici');
+var saltRounds = 10;
 
 module.exports.indexGet = function(req,res){
     res.render('login');
@@ -21,20 +23,19 @@ module.exports.signupGet = function(req,res){
 
 
 module.exports.signupPost = function(req,res){
-    try {
-        console.log('test');
-        var yeniKullanici = new Kullanici({
-            ad:req.body.name,
-            soyad:req.body.surname,
-            email:req.body.inputEmail,
-            kullaniciAdi:req.body.username,
-            sifre:req.body.inputPassword
-        });
-        yeniKullanici.save();
-        console.log(yeniKullanici);
+
+
+        bcrypt.hash(req.body.inputPassword, saltRounds, (err, hash) => {
+            var yeniKullanici = new Kullanici({
+                ad:req.body.name,
+                soyad:req.body.surname,
+                email:req.body.inputEmail,
+                kullaniciAdi:req.body.username,
+                sifre:hash
+            });
+            yeniKullanici.save();
+            console.log(yeniKullanici);
     
-        res.render('login');
-    } catch (error) {
-        console.log(error);
+        res.render('login',{ title: 'Registration Successfully!'});
+        });
     }
-}
